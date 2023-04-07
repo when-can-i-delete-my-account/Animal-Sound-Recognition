1c6aeff132c505044cbdf79658f6d8ede5933c20
when-can-i-delete-my-account <99968506+when-can-i-delete-my-account@users.noreply.github.com>
2023-04-07 19:57:49 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -8,3 +8,16 @@ function modelReady()
 {
     classifier.classify(gotResults);
 }
+var dog = 0;
+var lion = 0;
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+} else{
+    console.log(results);
+    
+
+    document.getElementById("sound").innerHTML = results[0].label;
+    document.getElementById("accuracy").innerHTML = (results[0].confidence*100).toFixed(0)+'% Accurate';
+}
+}
